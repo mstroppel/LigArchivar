5e6d6e355eb4b45e690ff4bc934adb4d0cf95da6
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-13 15:27:37 +0100

    #1 Activated StyleCop and FxCop

--- FL.LigArchivar/Bootstrapper.cs
+++ FL.LigArchivar/Bootstrapper.cs
@@ -6,15 +6,22 @@ using FL.LigArchivar.ViewModels;
 
 namespace FL.LigArchivar
 {
+    /// <summary>
+    /// Bootstrapper for this application. Builds up the Caliburn.Micro infrastructure.
+    /// </summary>
     public class Bootstrapper : BootstrapperBase
     {
         private SimpleContainer container;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bootstrapper"/> class.
+        /// </summary>
         public Bootstrapper()
         {
             Initialize();
         }
 
+        /// <inheritdoc/>
         protected override void Configure()
         {
             container = new SimpleContainer();
@@ -24,21 +31,25 @@ namespace FL.LigArchivar
             container.PerRequest<ShellViewModel>();
         }
 
+        /// <inheritdoc/>
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
         }
 
+        /// <inheritdoc/>
         protected override object GetInstance(Type service, string key)
         {
             return container.GetInstance(service, key);
         }
 
+        /// <inheritdoc/>
         protected override IEnumerable<object> GetAllInstances(Type service)
         {
             return container.GetAllInstances(service);
         }
 
+        /// <inheritdoc/>
         protected override void BuildUp(object instance)
         {
             container.BuildUp(instance);
