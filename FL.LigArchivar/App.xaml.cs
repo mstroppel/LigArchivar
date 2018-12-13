f56d850fa44f3a8a0a078a92567a78119dbd0541
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-13 15:27:37 +0100

    #1 Activated StyleCop and FxCop

--- FL.LigArchivar/App.xaml.cs
+++ FL.LigArchivar/App.xaml.cs
@@ -1,10 +1,15 @@
 ﻿namespace FL.LigArchivar
 {
+    /// <summary>
+    /// Interaction logic for "App.xaml"
+    /// </summary>
     public partial class App
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App" /> class.
+        /// </summary>
         public App()
         {
-            
         }
     }
 }
--- FL.LigArchivar/ViewModels/ShellViewModel.cs
+++ FL.LigArchivar/ViewModels/ShellViewModel.cs
@@ -2,6 +2,10 @@
 
 namespace FL.LigArchivar.ViewModels
 {
+    /// <summary>
+    /// View model for the main shell.
+    /// </summary>
+    /// <seealso cref="Screen" />
     public class ShellViewModel : Screen
     {
     }
