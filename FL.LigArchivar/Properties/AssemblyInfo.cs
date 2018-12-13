f51383467200fedf1cf1c5d2b5d012970290ee25
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-13 16:14:16 +0100

    #4 activated log4net

--- FL.LigArchivar/Properties/AssemblyInfo.cs
+++ FL.LigArchivar/Properties/AssemblyInfo.cs
@@ -29,3 +29,6 @@ using System.Windows;
                                               // (used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+// Configuer log4net by App.config
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
