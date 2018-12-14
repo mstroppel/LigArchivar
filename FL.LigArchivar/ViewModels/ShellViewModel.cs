2e3274f0bb0f815b850a5cadfa118df1455133a1
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-14 13:41:22 +0100

    Removed open settings dialog. Started with shell implementation.

--- FL.LigArchivar/ViewModels/ShellViewModel.cs
+++ FL.LigArchivar/ViewModels/ShellViewModel.cs
@@ -8,5 +8,30 @@ namespace FL.LigArchivar.ViewModels
     /// <seealso cref="Screen" />
     public class ShellViewModel : Screen
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShellViewModel"/> class.
+        /// </summary>
+        public ShellViewModel()
+        {
+            _rootDirectory = Properties.Settings.Default.RootDirectory;
+        }
+
+        /// <summary>
+        /// Gets or sets the root directory.
+        /// </summary>
+        /// <value>
+        /// The root directory.
+        /// </value>
+        public string RootDirectory
+        {
+            get { return _rootDirectory; }
+            set
+            {
+                _rootDirectory = value;
+                NotifyOfPropertyChange(nameof(RootDirectory));
+            }
+        }
+
+        private string _rootDirectory;
     }
 }
