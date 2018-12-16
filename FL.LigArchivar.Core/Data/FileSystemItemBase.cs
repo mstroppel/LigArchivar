29d8d5289d1886299bfeb8a015920070ca4518a7
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-16 21:26:33 +0100

    Refactoring of creating children.

---
+++ FL.LigArchivar.Core/Data/FileSystemItemBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using Caliburn.Micro;
+using FL.LigArchivar.Core.Utilities;
+
+namespace FL.LigArchivar.Core.Data
+{
+    public abstract class FileSystemItemBase : IFileSystemItem
+    {
+        public FileSystemItemBase(DirectoryInfo directory, string name, bool valid, TryCreateFileSystemItem tryCreateChild = null)
+        {
+            Directory = directory;
+            Name = name;
+            Valid = valid;
+            if (tryCreateChild != null)
+            {
+                Children = directory.GetChildrenFileSystemItems(tryCreateChild);
+            }
+            else
+            {
+                Children = ImmutableList<IFileSystemItem>.Empty;
+            }
+        }
+
+        public DirectoryInfo Directory { get; }
+
+        public string Name { get; }
+
+        public bool Valid { get; }
+
+        public IImmutableList<IFileSystemItem> Children { get; }
+    }
+}
