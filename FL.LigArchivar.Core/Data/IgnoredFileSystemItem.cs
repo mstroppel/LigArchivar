af24dbb2241884ab5773da8f0a1d3223bbfda364
Marco Stroppel MTA <marco.stroppel@mt.com>
2018-12-20 12:31:21 +0100

    Added possiblity to ignore folders. Ignore folders with name "Ordnerstruktur".

---
+++ FL.LigArchivar.Core/Data/IgnoredFileSystemItem.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace FL.LigArchivar.Core.Data
+{
+    public class IgnoredFileSystemItem : FileSystemItemBase
+    {
+        private const string IgnoredPrefix = "<ignored>";
+
+        public IgnoredFileSystemItem(DirectoryInfo directory)
+            : base(directory, IgnoredPrefix + directory.Name, true)
+        {
+        }
+    }
+}
